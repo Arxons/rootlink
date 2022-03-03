c049dd66e8ae22b85e77567681610e9017b9e085
Stanislav Vasyrin <vasyrin1312@gmail.com>
2022-03-03 16:56:51 +0300

    feat: done auth forms

---
+++ src/svg/LockSVG.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+export default function LockSVG(): JSX.Element {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      className="lock-svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="100px"
+      height="100px"
+      viewBox="0 0 100 100"
+      enableBackground="new 0 0 100 100"
+      xmlSpace="preserve"
+    >
+      {' '}
+      <image
+        id="image0"
+        width="100"
+        height="100"
+        x="0"
+        y="0"
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAQAAADa613fAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+RQfmAwMSDzWXVSjzAAAGS0lEQVR42u1bXWwVRRg92wq1tGDTPwwEQZQriQjUtFz+iqikUBI1TZRo
+QoLxSaytRBN9NL6YqAkJRn0yQSD+oqYNVQkIpVhaKJRKQqQtMQJGQAUEKb1QbI8PnR323pZ2Zndm
+tw/3vGx7M98539n53ZldII000kgjjTT8w7FDyxyUYxnmIIZi5ALowZ84ieNoQrNzLWrTahYcruQX
+TPB2SPBzrqSlG2jORhU7qIKjfCrqXG9vYiZ3Kplw8QPvjTrn4Wys4ZWkNLv5AZ9lCfM5juNYwBI+
+xw95MqnMZT4Tdd6pNt7ypHeTWxm/bcmF3MabntJvRp27N7lNnsQaeN+o5e/nd56ITVHn76Z1qzZ6
+uE456nleG1O1wjUynXOcrxVZwnMy9umobcyUXfwcZ2lHz+J52e2jHcHkgNujVxsyvkQ2sO+jtFEl
+m4Zy3xjC8YLkeDI6I+3u1BaIxR3BOiJauHClnDdGHXBH5InxP8FUEY2RL4X8lsBMnwqmz6KwkStX
+uPHAXIsFUy8nhG9klbumMsDl8NegjSvDt/oycd0V3IhDyfJI+EbmiOuB4EYANKewhmgkJq4njBhx
+WWKBWPyAF0SrLjDCViTY/g7fyA0hPd4IW5Zgu+6XwfdcSgoCQ7NxUD7/fWSM4Q4bpJyEMsTxIGZg
+BrIxCcC/SOAUTuE42tDmXI3atjdZgZRfi1nLH9k34u5JH3ezlkUqfJEY4WJ+LYeA0XGD27lozBnh
+Au5RtuDFbpaZMWJg1GIh3sW6lGHjBJpwDN04hX/QAyAX+ZiOGOZhOWYnlRzAJ3jDuWB6FNSuEa72
+bCGQZDtf4ZQRI6dyA48mxZxlZeRNiwOev+q4QDk+zvqk2MiNuGjlw9ocpTyUShOtkV6up6+plRms
+Zm+ERpjj0f6FvhffAMC57PSwhfmMyAx+K4WbeFdgvjzul3zf+Ktbf8LvSNkdvNMIYzYbJOfbYdmo
+kGPMPjM2hBW3Vga4OgwbhTwr+0bgRpXEnCf7yh9mHtdGltssxBKcZ5x7jhzBPrZto5T9Qmq9Ff5q
+wd6vPyvpCe2T05+VsYUZbBMKe2zaiMvuaO1+sUwOJWX2jGwXEnXWJABwh1D5ypZAkXzyU14a+tJZ
+KFT6WGhHwO2IR2zaAAD+LJReVI/R6bLu4f4220awVVzXWODmBPk8PiU42yha04TSdWabJ3/cnc9t
+2wAAdgu15aoR6k3LPc5p0kgni7U8yB728CBrmKXhpFFcF5q/R1t0OyCnyk47iA5OVY59ScRsNm+k
+RVA/qlg+K8XGoBXFWpENuVmtvI6R04J6hmL5Wg6HlxWjZ4ryv5k3clFQKy6wh24rDK7RFKMLRfkL
+5o1onofw6rBGFLev5XnJDdX87D0fZw77q+4+ovKuiroR915OVCx/XOPXoXBVlA8g9I1MUiz/kcav
+Q+GqmD9JYatotY8pls+UEbfQwkzF6BUiQvnwW71GusT1AbXiTj8qkTxGtWC106+o5qp0KZb3YWSu
+aoBzGUuxVv67FuXOZWU1V0XZiDJYISpb8wUBt1VpRrmLxhXmjeTImUR5xeTXCO8RQQn1Zbxy03Ku
+4ZD40/7b065Cq5OwwM4N4j61a0X5qZFjeiszXSPFcvNB41Uz/k6SPKMRsUhuPhSpR+lZqRcS9Rox
+lTzDM1ylEeHuy9dZsgGwXEgMsNSaRlxu0C2xZgRgkxA5rDpHa/JnyOX/Xos2AC6T96vaCn+NrPOl
+Vo0A3CbH+BLj3A/JYwXzz+pDxO7mJSHWyTyjzPlyPr/IYutGAD4hm1crc4yxZvMn2ayqQrABAHxf
+LswbzOwFMtvzjc/GkGwAHM9dUnZ/8AbGfDZLvp0cF5oRgBN5WEp3UnlpPyzXfNk3yDbmhmgDAFjo
+sdLLap+vcGSy1vMVaZulE5FRksj1NDCyTf+ojHEe8TDsDL02ZCLjuSnpVaUdVN505iI2JMVuDLVv
+DJNQlZxXBtHBVzltxIhpfC1lX/hi8C93Dbx4x8l4D2tTmLqxD8fQhdO4JF4FLMB0xDAfy5H8dRyx
+Fa87f1m4x77MlMsTeD00Wl3j+jSzlPWjvPHrRR/rxqAJaaaItWzk9REtJLiXNaYHWisvpzIbS1CK
+GGZjMvKQC+AqruA8utCFdhywsqWQRhpppJFGGgHwP7omVVMhenYEAAAAJXRFWHRkYXRlOmNyZWF0
+ZQAyMDIyLTAzLTAzVDE1OjE1OjUzKzAzOjAwZGhg3wAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMi0w
+My0wM1QxNToxNTo1MyswMzowMBU12GMAAAAASUVORK5CYII="
+      />
+    </svg>
+  );
+}
---
+++ src/svg/MinUserSVG.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export default function MinUserSVG(): JSX.Element {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      className="min-user-svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      width="100px"
+      height="100px"
+      viewBox="0 0 100 100"
+      enableBackground="new 0 0 100 100"
+      xmlSpace="preserve"
+    >
+      {' '}
+      <image
+        id="image0"
+        width="100"
+        height="100"
+        x="0"
+        y="0"
+        href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAQAAADa613fAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElN
+RQfmAwMROiUqlUR4AAAF/ElEQVR42u1bbWhWZRi+T3Nf77Jtuba2Fq35USarVdP8YlqNwBlRRIJg
+tB+ygkoYEsOwH5pR/TGNhBIJIrFQtoqoYAOLckmNIZKlbbPah8NVk9dtb/tw7uqH93l23uPes3Oe
+53nfZ4P3/nOPd899Pdd1zvNxPx+HKGlJS1rS5pJZ8a8CmZRLuTRGYQpbE3NOCBbSBiqn+2gZZTp+
+Pk8tdIIarYF41atTQg524DS8bAT7kGOap7eIEN7AZfixTtxtmm1sGWvRMQ3lYfTiDDoQdv3egydx
+o2nO08mowYSDZhhH8TxWO5sQ0rEOu9HnKDWGL7DSNPNoGS9gUtD7Bc8iNWbJEPa53k0DbjbN3yZX
+iSuiI7+ClBnLH3FJ6cI9pjUQEXJFcxlEpa+IENZgGxow7pBym2kdhDeZzAQeCRhZjGNCSrNpGfmI
+MJU9So8BeNqskDqmcRFZUvEWvmKEVrNC2pjGq9IIi8TAfb85GXk87E6iRAHFfievySLcoKxkFSee
+Z6y/FFAa2Psa8+IjpIz9T0ooLezvNSekhP1vSijn6QoREeXLZl/qQorY96mAWBN0kf/MMyUkg/0l
+RZxJNUbqQtLYqy5i7bWqMSG6VuHGhQyxv0kRZ74LL+FCwuyzVUCQQzlERDRO/aaEXGCvloTfaaNZ
+k3IA6kK6XVTkrIR9jyyAupDf2S9RQilnf1qZj6whm5PGwZkXuB4oTZw0bjEmhAidTGKZNIKFS4xx
+lyyGetMiamO/QhqhnHKJiKif2meDkOXSCNXsmy2YFNLL/iFlIT9qYCNrSMcfYg8lVwqhEFcZ4Wzs
+bb34C6lhEhG8LDduIRV1Yh9mszkhHzGFnUooOxnlQ1kE9T6ygP2vSij2+lJyWaVDSIR9kRJKIfsR
+c0LsFEV+FiGaGvHOKvORNWzk1t2LNGmMNPQyygZzQkIYYhK7kB983EIK8rGLEYaQGTRep5R3Hecc
+jYGjGx3R+w3KIEIe+gWVtsDRbSK2H9Jjli4pS9HA29CjSA8UmYEx3jn+HEsNy2BKx/m5Bsq4sEb2
+TbpNR9J4zeyEb32gqHXsWwJFxdNQzc/2HHxfC4GFdo7aZJr/FKksceNhve+Yh8VJ8Gw5niYiwvtM
+q8l3xDcc8Ylp7tG0louB9Blf5atF+SrT3N3UPhPHootmLFuAP7n09/57VaKE3CEWSBE87lmyAgNi
+XanlAFTf8EtEVhft4D9D5P1Oisnu3vutUzo5aDN8zE+6zrPUU7qbldY3QkR+T67segfkN4DiLcTf
+gY327q1fiD+qlq9SRoXYh6OjnqXG2AfKlRMrxD5CG/YsNewqPQuF2Af+c17I7ewjnqXsI88CzNPO
+QIdhi8ifyjzL5Yk7kNtNc56OXjVGmd7JGct+KlKUGtO8o4mF8JbYVZ+Y+S4vSkVeBhyS28fXLyID
+W9EjaE2i1lfUE6J5AQPYDtUrB4oiCvE6/nbsTV313+rxHO+hXLPLeAelZkSswuEoKkBfsAuzWCFW
+JXajbMSjCVyfYD5qcQrRNo4PcEtgpCzUi01X29pRjwVBkYKLWIwDGHRVHVZpFijG22KhZVsEh+J4
+zRwPoFGMTbadw4tQnqGRia3XfT4ziS+Dbfj5q6oURxxfJADAGI6iSmeLRiUOY8Ql5hgW66vAQi2G
+oyroQD0KtD8tIkI2al3vZgT1KhdEpqCLcCIK+DtsjPfIgio0R9XZovw1A8rQ7QA8ibXxleCoeSV+
+cNTc7Z2/zQRW4fg26h9sTvQeFDY5ptswKmRhinBBwHyNWxMrgjkUiJvzQC9kTo+RilYBcUBLd5OT
+koL3BI+fJa55YJsI32tKhOCyV3B5KWhorphtm8y9DcEmRYxi/wZM+rFHTHv6JiQVKUtEgro7SNg8
+8SXbQdMSBKeDosv7byGoFG3yQdMCBKepE5jV7v/F3kWxD1+6LOUTV11mtYp7wY/5F2JPPd+aph9l
+x9lf10piztPopIWmWXtYh+W6MB1bSIRCptl62H+W65vHGE0LoVktgyjkvkcUq48ofUSREJv9DJOW
+tKQlLWlzyP4HNmXk6VNd3BUAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjItMDMtMDNUMTQ6NTg6Mzcr
+MDM6MDDO+Wz8AAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIyLTAzLTAzVDE0OjU4OjM3KzAzOjAwv6TU
+QAAAAABJRU5ErkJggg=="
+      />
+    </svg>
+  );
+}
